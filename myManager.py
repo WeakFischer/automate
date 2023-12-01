84335f7118ef45ad425d20b9d096ea0ae0b78325
adudumayo023 <adudumayo023@student.wethinkcode.co.za>
2023-12-01 09:53:42 +0200

    Removed the last unnecessary comments

--- myManager.py
+++ myManager.py
@@ -8,7 +8,3 @@ pyperclip - to get clipboard contents
 webbrowser.open("https://mail.google.com/mail/")                                                            #   opens my wethinkcode email
 webbrowser.open("https://1st.curriculum.wethinkco.de/fundamentals/1.0/")                                    #   opens my curicculum
 webbrowser.open("https://www.accuweather.com/en/za/durban/305605/hourly-weather-forecast/305605")           #   opens today's weather in durban
-
-
-# I AM ADDING THIS COMMENT TO CHECK IF I HAVE SET UP GIT CORRECTLY IN THE TERMINAL
-# I want to check if I still have my branching and merging skills on point.
